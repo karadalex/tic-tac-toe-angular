3c7171b7daf08f9eedf3c2ae8a8b129652252515
Alex Karadimos <karadalex@gmail.com>
2018-04-17 13:40:16 +0300

    Display message with which cell was clicked

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -8,12 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class GridComponent implements OnInit {
 
   cells:number[][];
+  message:string;
 
   constructor() { 
     this.cells = [[1,2,3],[4,5,6],[7,8,9]];
   }
 
   ngOnInit() {
+  }
+
+  cellClicked(cell:number) {
+    this.message = `Cell ${cell} was clicked`;
   }
 
 }
