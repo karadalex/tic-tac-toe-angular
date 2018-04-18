915f247e7985aebd7db406fee38eaf367e230cfe
Alex Karadimos <karadalex@gmail.com>
2018-04-18 13:02:03 +0300

    Record players' movements in a 3x3 array

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -9,11 +9,25 @@ export class GridComponent implements OnInit {
 
   cells:number[][];
   @Input() playerTurn:number; // 0 for player1, 1 for player2/computer
+  playerMoves:number[][];
 
   constructor() { 
     this.cells = [[1,2,3],[4,5,6],[7,8,9]];
+    this.playerMoves = [[0,0,0],[0,0,0],[0,0,0]];
   }
 
   ngOnInit() {
+  }
+
+  playerPlayed(row, cell) {
+    let rowIndex = this.cells.indexOf(row);
+    let cellIndex = this.cells[rowIndex].indexOf(cell);
+    if (this.playerTurn) {
+      this.playerMoves[rowIndex][cellIndex] = 1;
+    } else {
+      this.playerMoves[rowIndex][cellIndex] = -1;
+    }
+    console.log(this.playerMoves);
+    // this.checkWinner();
   }
 }
